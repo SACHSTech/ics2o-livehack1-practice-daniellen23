955f8de883fce98f5bd44bfb2a3e2afca95b9b3a
daniellen23 <danielle.nguyen23@ycdsbk12.ca>
2021-02-09 04:44:23 +0000

    f_to_c.py added script 2021-02-08

--- f_to_c.py
+++ f_to_c.py
@@ -1,1 +1,14 @@
+""""
+Name: f_to_c.py
 
+Purpose: This program will convert the user inputted fahrenheit degree and print into celsius. 
+
+Author: Nguyen.D
+
+Created: 2021-02-08
+"""
+#c=5/9(f-32)
+language = "python3"
+fahrenheit = int(input("Enter Fahrenheit: "))
+celsius = (fahrenheit - 31) * (5 / 9)
+print(fahrenheit, "degrees fahrenheit converted to celsius: ", int(celsius), "degrees")
