465832824a3f485d59f762ab4294d388988103aa
daniellen23 <danielle.nguyen23@ycdsbk12.ca>
2021-02-09 05:09:27 +0000

    DRAFT: days_hours.py unfinished 2021-02-08

--- days_hours.py
+++ days_hours.py
@@ -1,1 +1,12 @@
+"""
+Name: days_hours.py
 
+Purpose: This program will convert user inputted amount of hours into days and hours.
+
+Author: Nguyen.D 
+
+Created: 2021-02-08
+"""
+hours = int(input("Enter amount of hours: "))
+days = hours / 24 
+print(days)
