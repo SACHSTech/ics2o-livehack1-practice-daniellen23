ce20311853ca3fa452c42ba6b8b01c3b7f7c9d6e
daniellen23 <danielle.nguyen23@ycdsbk12.ca>
2021-02-11 06:43:57 +0000

    *unifinished* minutes_days.py 2021-02-11

--- days_hours.py
+++ days_hours.py
@@ -8,5 +8,7 @@ Author: Nguyen.D
 Created: 2021-02-08
 """
 hours = int(input("Enter amount of hours: "))
-days = hours / 24 
-print(days)
+days = hours / 24
+new_hours = hours % 24 
+print("Number of days: ", (int(days)))
+print("Number of hours: ", (int(new_hours)))
--- minutes_days.py
+++ minutes_days.py
@@ -1,1 +1,35 @@
+"""
+Name: minutes_days.py
 
+Purpose: This program will convert user inputted amount of minutes into days, hours and minutes.
+
+Author: Nguyen.D 
+
+Created: 2021-02-11
+"""
+"""
+minutes = int(input("Enter amount of minutes: "))
+days = minutes / 1440
+#days = 1440 minutes
+#hours = 60 minutes
+remaining_hours = minutes % 1440
+new_hours = remaining_hours / 3600
+#minutes = new_hours / 60
+new_minutes = new_hours % 60
+
+#remaining_minutes = new_hours % 60
+#new_minutes = remaining_minutes
+
+print("Number of days: ", (int(days)))
+print("Number of hours: ", (int(new_hours)))
+print("Number of minutes: ", (int(new_minutes)))
+"""
+
+minutes = int(input("Enter amount of minutes: "))
+days = minutes / 1440
+hours = days % 24
+minutes = hours % 60
+
+print("Number of days: ", (int(days)))
+print("Number of hours: ", (int(hours)))
+print("Number of minutes: ", (int(minutes)))
